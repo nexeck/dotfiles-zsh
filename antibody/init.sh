292f6dad5bc9a4add43523337e5b0fcacdcd7583
Marcel Beck <marcel@beck.im>
2017-09-24 19:18:53 +0200

    Move to rcm for dotfile management

--- antibody/init.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-antibody bundle <~/.dotfiles/antibody/plugins.txt >~/.zsh_plugins.sh
--- install.sh
+++ install.sh
@@ -6,5 +6,21 @@ fi
 
 set +e
 
+echo ""
+echo "› Dependencies:"
+if ! command -v brew > /dev/null 2>&1; then
+	echo "  › Installing Homebrew"
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "  › Homebrew already installed"
+fi
+
+echo "› Dotfiles:"
+echo "  › Clone dotfiles"
 git clone git@github.com:nexeck/dotfiles.git ~/.dotfiles
-(cd ~/.dotfiles && sh ./bootstrap.sh)
+echo "  › Install rcm"
+brew tap thoughtbot/formulae
+brew install rcm
+
+echo "  › Install dotfiles"
+env RCRC=$HOME/.dotfiles/rcrc rcup
