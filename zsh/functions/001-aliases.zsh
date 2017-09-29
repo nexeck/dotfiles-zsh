b8f7b82367931b58f8d03c3f0af26b8972be225b
Marcel Beck <marcel@beck.im>
2017-09-29 19:18:29 +0200

    Add diff and exa aliases

--- zsh/functions/001-aliases.zsh
+++ zsh/functions/001-aliases.zsh
@@ -1,9 +1,14 @@
 #!/usr/bin/env zsh
 
-diff() {
-  if command -v git > /dev/null 2>&1; then
-    git --no-pager diff --color=auto --no-ext-diff --no-index "$@"
-  else
-    command diff --unified "$@"
-  fi
-}
+if command -v git > /dev/null 2>&1; then
+  alias diff='git --no-pager diff --color=auto --no-ext-diff --no-index'
+else
+  alias diff='diff --unified'
+fi
+
+if command -v exa > /dev/null 2>&1; then
+  alias ls='exa --group-directories-first'
+  alias lla='exa -l -a --group-directories-first'
+else
+  alias lla='ls -al'
+fi
