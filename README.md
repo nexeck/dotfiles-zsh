8ede6d6d3a1f81ef7a3cc0d97b9da5ebc0d5d3b7
Marcel Beck <marcel@beck.im>
2017-10-02 18:58:06 +0200

    Add install script

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # dotfiles
 
+## Dependencies
+
+Install homebrew
+
+## Install
 ```
 curl --silent https://raw.githubusercontent.com/nexeck/dotfiles/master/install.sh | sh
 ```
---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+
+set +e
+
+if [ "$(uname -s)" != "Darwin" ]; then
+	exit 0
+fi
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+echo "› Dotfiles:"
+echo "  › Clone dotfiles"
+if ! test -d ~/.dotfiles
+then
+  git clone git@github.com:nexeck/dotfiles.git ~/.dotfiles
+else
+  echo "  › Dotfiles exists"
+fi
+
+echo "  › Install rcm"
+brew tap thoughtbot/formulae
+brew install rcm
+
+echo "  › Install dotfiles"
+env RCRC=$HOME/.dotfiles/rcrc rcup
+
+echo "› Brew bundle"
+brew bundle --global
+
+echo "› ZSH"
+echo "  › Add ZSH to /etc/shells"
+test -n $(command -v zsh) && ! grep -qF "$(command -v zsh)" /etc/shells && command -v zsh | sudo tee -a /etc/shells
+echo "  › Switch default Shell to ZSH"
+chsh -s "$(command -v zsh)"
