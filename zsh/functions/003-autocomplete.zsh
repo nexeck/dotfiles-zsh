9fc02a98717c3cd4a0d1acd5c013744b212f7b08
Marcel Beck <marcel@beck.im>
2017-12-15 22:16:18 +0100

    Add autocomplete function

---
+++ zsh/functions/003-autocomplete.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+if command -v kubectl > /dev/null 2>&1; then
+  source <(kubectl completion zsh)
+fi
+
+if command -v gopass > /dev/null 2>&1; then
+  source <(gopass completion zsh)
+fi
+
+if command -v helm > /dev/null 2>&1; then
+  source <(helm completion zsh)
+fi
