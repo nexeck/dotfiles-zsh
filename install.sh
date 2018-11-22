80ea98f4174dfe921b0d3c5e6cd65f856bb53fe9
Marcel Beck <marcel@beck.im>
2018-11-22 23:02:35 +0100

    Tweak install script
    - Install homebrew
    - Clone Repo with https URL
    - Install ZSH

--- install.sh
+++ install.sh
@@ -12,13 +12,24 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+echo "› Update Software:"
+sudo softwareupdate -i -a
+
+echo "› Homebrew"
+if ! command -v brew > /dev/null 2>&1; then
+	echo "  › Installing Homebrew"
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "  › Homebrew already installed"
+fi
+
 echo "› Dotfiles:"
 echo "  › Clone dotfiles"
 if ! test -d ~/.dotfiles
 then
-  git clone git@github.com:nexeck/dotfiles.git ~/.dotfiles
+  git clone https://github.com/nexeck/dotfiles.git ~/.dotfiles
 else
-  echo "  › Dotfiles exists"
+  echo "  › Dotfiles already exists"
   exit 1
 fi
 
@@ -29,11 +40,12 @@ brew install rcm
 echo "  › Install dotfiles"
 env RCRC=$HOME/.dotfiles/rcrc rcup
 
-echo "› Brew bundle"
-brew bundle --global
-
 echo "› ZSH"
+brew install zsh
 echo "  › Add ZSH to /etc/shells"
 test -n $(command -v zsh) && ! grep -qF "$(command -v zsh)" /etc/shells && command -v zsh | sudo tee -a /etc/shells
 echo "  › Switch default Shell to ZSH"
 chsh -s "$(command -v zsh)"
+
+echo "› Brew bundle"
+brew bundle --global
