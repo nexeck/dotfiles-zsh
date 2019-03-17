ac0f287c2840fbd46c9b061a66049a10b69131c6
Marcel Beck <marcel@beck.im>
2019-03-17 13:36:09 +0100

    feat: Use nvim as visual editor
    This prevents other editors to open when you use git commit in your
    shell

--- zsh/config/editor.zsh
+++ zsh/config/editor.zsh
@@ -3,12 +3,9 @@
 # Editors {{{
 if command -v nvim > /dev/null 2>&1; then
   export EDITOR='nvim'
+  export VISUAL='nvim'
 else
   export EDITOR='vim'
-fi
-if command -v atom > /dev/null 2>&1; then
-  export VISUAL='atom'
-else
   export VISUAL='vim'
 fi
 # }}}
